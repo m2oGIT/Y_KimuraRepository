48b4870db8c006200d709a89614fbf6a3cff0ce7
test0723 <k-mogi@eiwa.com>
2016-07-23 15:46:03 +0900

    Test05：新規作成

---
+++ Test/src/Java001/Test05.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2016 EIS Co., Ltd. All rights reserved.
+ */
+
+package Java001;
+
+/**
+ * @author （y-kimura） <br />
+ *         （Test05） <br />
+ *         （複数の文字列を起動時引数に渡してコンソール表示） <br />
+ *         更新履歴 2016/7/23 （y-kimura）：（新規作成） <br />
+ */
+public class Test05 {
+
+  /**
+   * （起動時引数を複数渡して表示） <br />
+   * （起動時引数を複数渡して表示） <br />
+   * 
+   * @param args 実行時引数3件
+   */
+  public static void main( String[] args ) {
+    System.out.println( args[0] );
+    System.out.println( args[1] );
+    System.out.println( args[2] );
+
+  }
+
+}
