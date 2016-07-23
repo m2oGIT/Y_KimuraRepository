708323f248c2a2f1bc1cb90f2e2e745f921dbaa6
test0723 <k-mogi@eiwa.com>
2016-07-23 15:00:41 +0900

    start

---
+++ Test/src/Pack01/Test01.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2016 EIS Co., Ltd. All rights reserved.
+ */
+
+package Pack01;
+
+/**
+ * @author （作成者） <br />
+ *         （クラス論理名） <br />
+ *         （説明） <br />
+ *         更新履歴 yyyy/mm/dd （更新者）：（説明） <br />
+ */
+public class Test01 {
+
+  /**
+   * （メソッド論理名） <br />
+   * （説明） <br />
+   * 
+   * @param args
+   */
+  public static void main( String[] args ) {
+    System.out.println( "Hello World!!" );
+
+  }
+
+}
---
+++ Test/src/Pack01/Test02.java
@@ -0,0 +1,22 @@
+
+package Pack01;
+
+/**
+ * @author （作成者） <br />
+ *  （クラス論理名） <br />
+ *  （説明） <br />
+ *  更新履歴 yyyy/mm/dd （更新者）：（説明） <br />
+ */
+public class Test02 {
+
+  /**
+   * （メソッド論理名） <br />
+   * （説明） <br />
+   * @param args 実行時引数
+   */
+  public static void main( String[] args ) {
+    System.out.println( args[0] );
+
+  }
+
+}
