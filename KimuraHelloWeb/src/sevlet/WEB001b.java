ca74aa299f838b630d6445d7096b8e0691c81cf8
kimurayuki <kimurayuki@DESKTOP-CF1V9GG>
2017-04-09 12:56:42 +0900

    2017/4/9 update servletの基本 問２

--- KimuraHelloWeb/src/sevlet/WEB001b.java
+++ KimuraHelloWeb/src/sevlet/WEB001b.java
@@ -2,6 +2,7 @@ package sevlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,21 +11,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class WEB001a
+ * Servlet implementation class WEB001b
  */
-@WebServlet("/WEB001a")
-public class WEB001a extends javax.servlet.http.HttpServlet {
+@WebServlet("/WEB001b")
+public class WEB001b extends javax.servlet.http.HttpServlet {
   private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public WEB001a() {
+    public WEB001b() {
         super();
         // TODO Auto-generated constructor stub
-        // WEB001a 問１
-        // ブラウザから入力値を２つ取り、第１入力値の文字列を
-        // 第２入力値の回数分繰り返し表示する
+        // WEB001b 問２
+        // ブラウザから入力値を1つ取り、複数人の身長を読み込み、標準体重を表示する
     }
 
   /**
@@ -38,21 +38,36 @@ public class WEB001a extends javax.servlet.http.HttpServlet {
       PrintWriter pw = response.getWriter();
       pw.println("<html>");
       pw.println("<head>");
-      pw.println("<title>WEB001a</title>");
+      pw.println("<title>WEB001b</title>");
       pw.println("</head>");
       pw.println("<body>");
       // JavaプログラムSTART
-      // 入力値１：繰り返し表示する文字列
-      String input = request.getParameter("param1");
-      // 入力値２：繰り返す回数
-      String str = request.getParameter("param2");
-      // 入力値２を数値に変換
-      int n = Integer.MIN_VALUE;
-      n = Integer.parseInt(str);
-      // 入力値１を入力値２の回数分表示する
-      for (int i = 0; i < n; i++) {
-        pw.print(input);
-        System.out.print(input);
+      // 入力値１：身長（カンマ区切り）
+      String str = request.getParameter("height");
+      // カンマ区切りを処理する
+      String comma = ",";
+      StringTokenizer height =new StringTokenizer(str,comma);
+      // カンマ区切りをリストに格納する
+      int list[] = new int[height.countTokens()];
+      // データ数の分だけ配列に入れる
+      int n = 0;
+      while (height.hasMoreTokens()) {
+        list[n] = Integer.parseInt(height.nextToken());
+        n++;
+      }
+      // 配列から身長を取り出し、体重を計算して表示する
+      double weight =0.00000;
+      for (int i = 0; i < list.length; i++) {
+        weight = (list[i] - 100) * 0.9;
+        // 小数点以下５桁を表示
+        pw.printf("%.5f",weight);
+        // 改行　←できない！！(2017/4/9)
+        pw.println("\r\n");
+        // 小数点以下５桁を表示
+        System.out.printf("%.5f",weight);
+        System.out.println("\r\n");
+        // 初期化
+        weight =0.00000;
       }
       // JavaプログラムEND
       pw.println("</body>");
